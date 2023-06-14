d383115864dfa8ce9903fcbb86da1482a40c28b3
Mads Nymann <mads433f@edu.zealand.dk>
2023-06-14 12:27:26 +0200

    Små ændringer

--- BaalhytteBooking/Reservationer.cs
+++ BaalhytteBooking/Reservationer.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BaalhytteBooking
 {
-    internal class Reservationer
+    public class Reservationer
     {
+        public Dictionary<int, Reservation> reservation { get; set; }
+
+        #region Instance Fields
+        private int _id;
+        #endregion
+
+        #region Constructor
+        public Reservationer()
+        {
+            reservation = new Dictionary<int, Reservation>();
+        }
+        #endregion
+
+        #region Properties
+        public int Id { get { return _id; } set { _id = value; } }
+        #endregion
+
+        #region Methods
+
+        public void RegistrerReservation(Reservation reservation)
+        {
+        }
+
+        public void FjernReservation(Reservation reservation)
+        {
+
+        }
+
+        public void PrintReservationer()
+        {
+            Console.WriteLine();
+        }
+
+        #endregion
     }
 }
+
