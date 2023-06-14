19fe006fcc824c9b71d0b662b33daf37b0114eff
Mads Nymann <mads433f@edu.zealand.dk>
2023-06-14 09:33:03 +0200

    BoerneGruppe klassen

---
+++ BaalhytteBooking/BoerneGruppe.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaalhytteBooking
+{
+    public class BoerneGruppe
+    {
+        private int _id;
+        private string _name;
+        private string _aldersgruppe;
+        private int _antalAfGrupper;
+
+        #region Constructor
+
+        public BoerneGruppe(int _id, string _name, string _aldersgruppe, int _antalAfGrupper) {
+            ID = _id;
+            Name = _name;
+            Aldersgruppe = _aldersgruppe;
+            AntalAfGrupper = _antalAfGrupper;
+        }
+        #endregion
+
+        #region Properties
+        public int ID { 
+            get { return _id; } 
+            set { _id = value; }
+        }
+        public string Name { 
+            get { return _name;} 
+            set { _name = value; }
+        }
+        public string Aldersgruppe { 
+            get { return _aldersgruppe;}
+            set { _aldersgruppe = value;}
+        }
+        public int AntalAfGrupper { 
+            get { return _antalAfGrupper; }
+            set
+            {
+                _antalAfGrupper = value;
+            }
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"ID:{ID}, Navn:{Name}, Aldersgruppe:{Aldersgruppe}, Deltagere:{AntalAfGrupper}";
+        }
+    }
+}
--- BaalhytteBooking/Program.cs
+++ BaalhytteBooking/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BaalhytteBooking;
 
-Console.WriteLine("Mads");
+Console.WriteLine("Opgave 1");
+Console.WriteLine("Test BoerneGruppe klassen");
+
+BoerneGruppe b1 = new BoerneGruppe(1, "Mads", "Børn", 2);
+BoerneGruppe b2 = new BoerneGruppe(2, "Mads 2", "Voksne", 10);
+
+Console.WriteLine(b1);
+Console.WriteLine(b2);
+Console.WriteLine("________________________________________________________________________________________");
